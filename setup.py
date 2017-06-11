cd43ed6663da81902e5734a7e8d1b257d908516d
Mads Hartmann <mads379@gmail.com>
2017-06-11 16:11:04 +0200

    Two == seems to be the right amount

--- setup.py
+++ setup.py
@@ -21,21 +21,21 @@ setup(
     packages=find_packages(exclude=['contrib', 'docs', 'tests']),
     package_data={'': ['README.md']},
     install_requires=[
-        'boto===2.47.0',
-        'boto3===1.4.4',
+        'boto==2.47.0',
+        'boto3==1.4.4',
         'botocore==1.5.55',
-        'PyYAML===3.12',
+        'PyYAML==3.12',
         'docopt==0.6.2',
         'tornado==4.5.1'
     ],
     extras_require={
         'dev': [
-            'ptpython===0.39',
-            'autopep8===1.3.1',
-            'pylint===1.6.5',
-            'flake8===3.3.0',
+            'ptpython==0.39',
+            'autopep8==1.3.1',
+            'pylint==1.6.5',
+            'flake8==3.3.0',
             'flake8-print==2.0.2',
-            'pydocstyle===1.1.1',
+            'pydocstyle==1.1.1',
             'infi.docopt-completion==0.2.7'
         ]
     },
